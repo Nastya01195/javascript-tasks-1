e4383378557cae0d11a716b439d1e4c16fed2a95
Nastya01195 <nastya01195@mail.ru>
2015-10-07 18:58:56 +0500

    Исправленное

--- roman-time.js
+++ roman-time.js
@@ -1,183 +1,93 @@
 var hours = parseInt(process.argv[2],10);
 var minutes = parseInt(process.argv[3],10);
 var romanTime;
-var romanTimeScreen='';
-var romanTimeASCLL = new Array();
-var noFull =['','','','','','','',''];
+var romanTimeASCLLScreen='';
+var heightASCLL=8;
+
 if(hours<25 && hours>-1 && minutes<60 && minutes>-1){
 	romanTime=timeToRoman(romanTime);
-	for(var i=0;i< romanTime.length;i++){
-		romanTimeScreen+=romanTime[i];
+	console.log(romanTime);
+		for(var i=0;i< heightASCLL;i++){
+			for(var j=0;j<romanTime.length;j++){
+				romanTimeASCLLScreen+=timeRomanToASCLL(romanTime[j])[i];
+			}
+			romanTimeASCLLScreen+='\n';
 	}
-	console.log(romanTimeScreen);
-	romanTimeASCLL=timeRomanToASCLL(romanTimeASCLL,timeToRoman(romanTime));
-		for(var i=0;i< romanTimeASCLL.length;i++){
-		console.log(romanTimeASCLL[i]);
-	}
+	console.log(romanTimeASCLLScreen);
 }else {
 	console.log("Время указано не верно");}
+
 function timeToRoman(romanTime){
 	romanTime=[''];
+	romanTime=arabicNumberToRoman(hours);
+	romanTime+=':'+arabicNumberToRoman(minutes);
+	return romanTime;
+}
+function arabicNumberToRoman(number){
 	var units = ['','I','II','III','IV','V','VI','VII','VIII','IX'];
 	var dozens = ['','X','XX','XXX','XL','L'];
-	var countArray=0;
-	romanTime[countArray]=dozens[parseInt(hours/10,10)];
-	countArray++;
-	romanTime[countArray]=units[hours%10];
-	if(dozens[parseInt(hours/10),10]==0 && units[hours%10]==0){
-		countArray--;
-		romanTime[countArray]='-';
+	if(dozens[parseInt(number/10),10]==0 && units[number%10]==0){
+		return '-';
 	}
-	countArray++;
-	romanTime[countArray]=':';
-	countArray++;
-	romanTime[countArray]=dozens[parseInt(minutes/10,10)];
-	countArray++;
-	romanTime[countArray]=units[minutes%10];
-	if(dozens[parseInt(minutes/10,10)]==0 && units[minutes%10]==0){
-		countArray--;
-		romanTime[countArray]='-';
-	}
-	return(romanTime);
+	return romanTime=dozens[parseInt(number/10,10)]+units[number%10];
 }
-function timeRomanToASCLL(romanTimeASCLL,romanTime){
-	for(var i=0;i<noFull.length;i++){
-		romanTimeASCLL[i]=noFull[i];
-	} var count =0;
-	var O =['               ',
-		'               ',
-		'               ',
-		' _  _  _  _  _ ',
-		'(_)(_)(_)(_)(_)',
-		'               ',
-		'               ',
-		'               '];
-	var I = [' _ ',
-		'(_)',
-		'(_)',
-		'(_)',
-		'(_)',
-		'(_)',
-		'(_)',
-		'(_)'];
-	var V = [' _           _ ',
-		'(_)         (_)',
-		'(_)         (_)',
-		'(_)_       _(_)',
-		'  (_)     (_)  ',
-		'   (_)   (_)   ',
-		'    (_)_(_)    ',
-		'      (_)      '];
-	var X = [' _           _ ',
-		'(_)_       _(_)',
-		'  (_)_   _(_)  ',
-		'    (_)_(_)    ',
-		'     _(_)_     ',
-		'   _(_) (_)_   ',
-		' _(_)     (_)_ ',
-		'(_)         (_)'];
-	var L =[' _          ',
-		'(_)         ',
-		'(_)         ',
-		'(_)         ',
-		'(_)         ',
-		'(_)         ',
-		'(_) _  _  _ ',
-		'(_)(_)(_)(_)'];
-	var colon = ['   ',
-		     '   ',
-		     ' _ ',
-	       	     '(_)',
-		     '   ',
-		     ' _ ',
-		     '(_)',
-		     '   '];
-	do{
-		switch(romanTime[count]){
-			case('-'):
-				for (var i = 0; i < O.length; i++) {
-					romanTimeASCLL[i] += O[i];
-				}
+
+function timeRomanToASCLL(romanTime){
+		switch(romanTime){
+			case('-'): return ['               ',
+					   '               ',
+					   '               ',
+				      	   ' _  _  _  _  _ ',
+					   '(_)(_)(_)(_)(_)',
+					   '               ',
+					   '               ',
+					   '               '];
 				break;
-			case(':'):
-				for (var i = 0; i < colon.length; i++) {
-					romanTimeASCLL[i] += colon[i];
-				}
+			case(':'): return ['   ',
+					   '   ',
+					   ' _ ',
+					   '(_)',
+					   '   ',
+				  	   ' _ ',
+					   '(_)',
+					   '   '];
 				break;
-			case('I'):
-				for(var i=0;i< I.length;i++){
-					romanTimeASCLL[i]+=I[i];
-				}
+			case('I'): return [' _ ',
+					   '(_)',
+					   '(_)',
+					   '(_)',
+					   '(_)',
+				 	   '(_)',
+				 	   '(_)',
+					   '(_)'];
 				break;
-			case('II'):
-				for(var i=0;i< I.length;i++){
-					romanTimeASCLL[i]+=I[i]+I[i];
-				}
+			case('V'): return [' _           _ ',
+					   '(_)         (_)',
+					   '(_)         (_)',
+				 	   '(_)_       _(_)',
+				 	   '  (_)     (_)  ',
+					   '   (_)   (_)   ',
+					   '    (_)_(_)    ',
+					   '      (_)      '];
 				break;
-			case('III'):
-				for(var i=0;i< I.length;i++){
-					romanTimeASCLL[i]+=I[i]+I[i]+I[i];
-				}
+			case('X'): return [' _           _ ',
+					   '(_)_       _(_)',
+					   '  (_)_   _(_)  ',
+				   	   '    (_)_(_)    ',
+					   '     _(_)_     ',
+					   '   _(_) (_)_   ',
+					   ' _(_)     (_)_ ',
+					   '(_)         (_)'];
 				break;
-			case('IV'):
-				for(var i=0;i< I.length;i++){
-					romanTimeASCLL[i]+=I[i]+V[i];
-				}
-				break;
-			case('V'):
-				for(var i=0;i< V.length;i++){
-					romanTimeASCLL[i]+=V[i];
-				}
-				break;
-			case('VI'):
-				for(var i=0;i< V.length;i++){
-					romanTimeASCLL[i]+=V[i]+I[i];
-				}
-				break;
-			case('VII'):
-				for(var i=0;i< V.length;i++){
-					romanTimeASCLL[i]+=V[i]+I[i]+I[i];
-				}
-				break;
-			case('VIII'):
-				for(var i=0;i< V.length;i++){
-					romanTimeASCLL[i]+=V[i]+I[i]+I[i]+I[i];
-				}
-				break;
-			case('IX'):
-				for(var i=0;i< I.length;i++){
-					romanTimeASCLL[i]+=I[i]+X[i];
-				}
-				break;
-			case('X'):{
-				for(var i=0;i< X.length;i++){
-					romanTimeASCLL[i]+=X[i];
-				}
-				break;
-			}
-			case('XX'):
-				for(var i=0;i< X.length;i++){
-					romanTimeASCLL[i]+=X[i]+X[i];
-				}
-				break;
-			case('XXX'):
-				for(var i=0;i< X.length;i++){
-					romanTimeASCLL[i]+=X[i]+X[i]+X[i];
-				}
-				break;
-			case('XL'):
-				for(var i=0;i< X.length;i++){
-					romanTimeASCLL[i]+=X[i]+L[i];
-				}
-				break;
-			case('L'):
-				for(var i=0;i< L.length;i++){
-					romanTimeASCLL[i]+=L[i];
-				}
+			case('L'): [' _          ',
+				   '(_)         ',
+				   '(_)         ',
+				   '(_)         ',
+				   '(_)         ',
+				   '(_)         ',
+				   '(_) _  _  _ ',
+				   '(_)(_)(_)(_)'];
 				break;
 			default :
 		}
-		count++;
-	}while(count<romanTime.length);
-	return(romanTimeASCLL);
 }
