e5bd23d9fcc9a80061c25b6a3e130247b036bf60
Nastya01195 <nastya01195@mail.ru>
2015-10-08 23:09:57 +0500

    Исправленное3

--- roman-time.js
+++ roman-time.js
@@ -2,12 +2,14 @@ var hours = parseInt(process.argv[2],10);
 var minutes = parseInt(process.argv[3],10);
 var romanTimeASCLLScreen='';
 var heightASCLL=8;
+var romanTime;
 
 if(hours<25 && hours>-1 && minutes<60 && minutes>-1){
-	console.log(timeToRoman());
+	romanTime=timeToRoman();
+	console.log();
 		for(var i=0;i< heightASCLL;i++){
-			for(var j=0;j<timeToRoman().length;j++){
-				romanTimeASCLLScreen+=timeRomanToASCLL(timeToRoman()[j])[i];
+			for(var j=0;j<romanTime.length;j++){
+				romanTimeASCLLScreen+=timeRomanToASCLL(romanTime[j])[i];
 			}
 			romanTimeASCLLScreen+='\n';
 	}
@@ -16,15 +18,12 @@ if(hours<25 && hours>-1 && minutes<60 && minutes>-1){
 	console.log("Время указано не верно");}
 
 function timeToRoman(){
-	var romanTime;
-	romanTime=arabicNumberToRoman(hours);
-	romanTime+=':'+arabicNumberToRoman(minutes);
-	return romanTime;
+	return arabicNumberToRoman(hours)+':'+arabicNumberToRoman(minutes);
 }
 function arabicNumberToRoman(number){
 	var units = ['','I','II','III','IV','V','VI','VII','VIII','IX'];
 	var dozens = ['','X','XX','XXX','XL','L'];
-	if(dozens[parseInt(number/10),10]==0 && units[number%10]==0){
+	if(dozens[Math.floor(number/10),10]==0 && units[number%10]==0){
 		return '-';
 	}
 	return dozens[parseInt(number/10,10)]+units[number%10];
